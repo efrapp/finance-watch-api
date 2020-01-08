a26d2888098677d13550daa44a1924e7ded24974
Efrain Pinto <efrapp@gmail.com>
2020-01-08 16:29:44 -0500

    Build a web server

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
+const http = require('http');
 
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+
+  res.end('Request completed');
+}).listen(3001, 'localhost');
